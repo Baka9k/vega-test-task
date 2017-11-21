ea4769b4df899804069d06ea98907f124d9e70df
Baka9k <neoplanspaceliner9@gmail.com>
2017-11-21 18:57:49 +0300

    Set up auto-updating gh-pages branch

--- build/webpack.prod.config.js
+++ build/webpack.prod.config.js
@@ -1,12 +1,19 @@
 const webpack = require('webpack')
 const merge = require('webpack-merge')
 const common = require('./webpack.common.config.js')
+const GhPagesWebpackPlugin = require('gh-pages-webpack-plugin')
 
 module.exports = merge(common, {
   plugins: [
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('production')
     }),
-    new webpack.optimize.UglifyJsPlugin()
+    new webpack.optimize.UglifyJsPlugin(),
+    new GhPagesWebpackPlugin({
+      path: './dist',
+      options: {
+        message: 'Update gh-pages'
+      }
+    })
   ]
 })
